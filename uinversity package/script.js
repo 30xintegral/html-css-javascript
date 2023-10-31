70ca1f8dc495324069b333f420ad8fbd49ca2b4a
30xintegral <101065023+30xintegral@users.noreply.github.com>
2023-10-31 17:31:23 +0400

    Add files via upload

---
+++ uinversity package/script.js
@@ -0,0 +1,14 @@
+
+
+let a = 5;
+let b=9;
+console.log(a+b)
+
+let fgfg=document.querySelector(".name")
+
+document.querySelector(".btn").addEventListener("click",function(){
+    console.log("fdsfsf")
+    let text = document.querySelector(".b").innerText;
+    fgfg.innerText = text;
+    console.log(document.querySelector(".name").innerText);
+})
